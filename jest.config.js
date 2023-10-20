749ee62730c76e13cb3764c8a0aa87babbfec4f8
miqueiasvianasilva21 <miqueias@icomp.ufam.edu.br>
2023-10-20 13:58:18 -0400

    adicionando tudo

--- jest.config.js
+++ jest.config.js
@@ -107,7 +107,7 @@ const config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: ["./node_modules/jest-html-reporter"],
+  reporters: ["./node_modules/jest-html-reporter"],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
